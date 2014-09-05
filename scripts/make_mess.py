1d8eda412eda68fefc1d6efc4891d5eb6a4a633d
Kyle Barlow <kb@kylebarlow.com>
2014-09-05 10:38:32 -0700

    Updated shell activity with grepping

--- scripts/make_mess.py
+++ scripts/make_mess.py
@@ -21,18 +21,23 @@ bad_numbers = random.sample(xrange(100), random.randint(20, 40))
 
 numbers_lists = [good_numbers, bad_numbers]
 
-def make_data_files(output_dir):
+def make_data_files(output_dir, total_score=False):
     files_to_make = random.randint(10, 50)
     for x in xrange(1, files_to_make+1):
         output_file = os.path.join(output_dir, 'data_%04d.txt' % x)
         with open(output_file, 'w') as f:
             for line_num in xrange(0, random.randint(100, 500)):
                 f.write('Data point %06d=%.6f\n' % (line_num, random.random()) )
+            if total_score:
+                if random.random() >= 0.5:
+                    f.write('Total score: %.2f\n' % random.random())
+                else:
+                    f.write('SEGFAULT\n')
 
 for n in good_numbers:
     new_dir = os.path.join(data_dir, 'good_data_%d' % n)
     os.mkdir(new_dir)
-    make_data_files(new_dir)
+    make_data_files(new_dir, total_score=True)
 
 for n in bad_numbers:
     new_dir = os.path.join(data_dir, 'bad_data_%d' % n)
